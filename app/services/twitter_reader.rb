cbc79add21412c1f4313bb8f22ed1bddf69589e7
Stefan Wienert <stefan.wienert@pludoni.de>
2013-11-08 00:13:48 +0100

    #13 mostly done with thin wrapper to twitter. After adding #9 will continue

--- app/services/twitter_reader.rb
+++ app/services/twitter_reader.rb
@@ -1,5 +1,49 @@
 class TwitterReader
-  def self.twitter_accounts
-    User.with_twitter
+  class << self
+    def twitter_accounts
+      User.with_twitter
+    end
+
+    def cronjob
+      twitter_accounts.each do |ta|
+        new(ta).run
+      end
+    end
+
+    def client
+      Twitter::REST::Client.new do |config|
+        config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+        config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+        config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+        config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+      end
+    end
   end
+
+  def initialize(user)
+    @user = user
+    @twitter_account = user.twitter_account
+  end
+
+  def run
+    tweets.each do |tweet|
+      if tweet.text.include? '#event'
+        binding.pry
+        EventFactory.new.create( tweet.text, @user )
+      end
+    end
+  end
+
+
+  private
+
+  def tweets
+    self.class.client.user_timeline(@twitter_account,
+                                    exclude_replies: true,
+                                    trim_user: true,
+                                    include_rts: false)
+  end
+
+
+
 end
--- test/services/twitter_reader_test.rb
+++ test/services/twitter_reader_test.rb
@@ -1,9 +1,23 @@
 require 'test_helper'
 class TwitterReaderTest < ActiveSupport::TestCase
   fixtures :users
+
+  setup do
+    @user = users(:valid_user)
+    @user.update_attribute :twitter_account, '@ruby_dresden'
+  end
+
   test 'Twitter Reader finds all users with twitter accounts' do
-    user = users(:valid_user)
-    user.update_attribute :twitter_account, '@ruby_dresden'
     assert_equal 1, TwitterReader.twitter_accounts.count
+  end
+
+  test 'creates events' do
+    Event.delete_all
+    tr = TwitterReader.new(@user)
+    def tr.tweets; [
+      Twitter::Tweet.new(id: 123, text: 'Ruby Dresden am 17.11. um 17 Uhr - kommt alle! #event') ]; end
+
+    tr.run
+    # assert_equal 1, Event.count # TODO #9 muss erst gehen
   end
 end
