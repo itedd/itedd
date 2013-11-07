d8d8ad05f9c86fce1aa891b09937219a0503d144
soemo <soeren@mothes.org>
2013-11-07 22:20:11 +0100

    ExtractDate module

--- app/models/concerns/extract_date.rb
+++ app/models/concerns/extract_date.rb
@@ -6,14 +6,31 @@ module ExtractDate
     result = ''
 
     if string.present?
+      result = extract_with_numbers(string)
+      result = check_date(result)
+
+      result = extract_with_prosa(string) if result.nil?
+      result = check_date(result)
+    end
+
+    result
+  end
+
+
+
+  private
+
+  def extract_with_numbers(string)
+    result = ''
+
+    if string.present?
+
       matcher = string.match /([1-9]|0[1-9]|[12][0-9]|3[01])[-\.]([1-9]|0[1-9]|1[012])[-\.]([1-9][0-9][0-9][0-9]|[1-9][0-9])/
 
       day_string   =  matcher[1]
       month_string =  matcher[2]
       year_string  =  matcher[3]
 
-
-      # refactoring
       if day_string.present?
         if day_string.start_with?('0')
           result << "#{day_string[1..-1]}"
@@ -21,7 +38,7 @@ module ExtractDate
           result << "#{day_string}"
         end
       end
-      # refactoring
+
       if month_string.present?
         if month_string.start_with?('0')
           result << ".#{month_string[1..-1]}"
@@ -32,17 +49,41 @@ module ExtractDate
 
       if year_string.present?
         length = year_string.length
-        if length == 4
-          result << ".#{year_string}"
-        elsif length == 2
-          #todo universeller machen, gilt erstmal nur 2000 bis 2099
-          result << ".20#{year_string}"
-        else
-          # Krawumm was dann ?
+        case length
+          when 4
+            result << ".#{year_string}"
+          when 2
+            #todo universeller machen, gilt erstmal nur 2000 bis 2099
+            result << ".20#{year_string}"
+          else
+            # Krawumm was dann ?
         end
-
+      else
+        # Jahr selbst anhängen
+        #Bei Ausfuehrung am 1.7.2013:
+        #"1.8. tolles Event www.example.com #event" -> 1.8.2013
+        #"1.6. tolles Event www.example.com #event" -> 1.6.2014
       end
+    end
 
+    result
+  end
+
+  def extract_with_prosa(string)
+    # TODO z.B. '1. Mai ' oder  'Erster Januar' erkennen
+    result = ''
+  end
+
+  def check_date(date_string)
+    result = nil
+
+    if date_string.present?
+      begin
+        date_string.to_date
+        result = date_string
+      rescue ArgumentError
+        result = nil
+      end
     end
 
     result
