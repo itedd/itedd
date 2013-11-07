c266b40a1cee0d59e98772eb367fcc98af273419
dgrawunder <dev.dgr@gmail.com>
2013-11-07 20:21:55 +0100

    resolved #7

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  should belong_to(:user)
+  should validate_presence_of(:happens_at)
+  should validate_presence_of(:link)
+  should validate_presence_of(:text)
+  should validate_presence_of(:user)
+  should ensure_length_of(:text).is_at_most(200)
+  should ensure_length_of(:link).is_at_most(200)
+
 end
