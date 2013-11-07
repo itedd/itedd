bb0923cb2c11fa135738f4326429ed1b6c32b622
Peter Brändel <peter@bitbites.de>
2013-11-07 20:11:53 +0100

    Added secret_key to application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Itedd
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.secret_key_base = "7aad3dee5f40dbb365b8c440d8c23168ea3ad45bf3cb6da2b3c5a6ab3ed60d22870adf115d7486def34c90ca5b1b0618a19a86c1739887c15608fa6c24b74cdd"
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
