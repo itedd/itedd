55baf83d33b0368b96ff7cc83a257c5b9ec35523
dgrawunder <dev.dgr@gmail.com>
2014-02-06 20:36:30 +0100

    set default local

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,25 +18,25 @@ Event.create! text: 'Ruby User Group Dresden Event 1',
               happens_at: 4.weeks.from_now,
               organizer: User.first,
               link: 'http://',
-              twitter_id: Array.new(32) { rand(36).to_s(36) }.join
+              twitter_id: fake_twitter_id
 
 Event.create! text: 'Ruby User Group Dresden Event 2',
               happens_at: 8.weeks.from_now,
               organizer: User.first,
               link: 'http://',
-              twitter_id: Array.new(32) { rand(36).to_s(36) }.join
+              twitter_id: fake_twitter_id
 
 Event.create! text: 'Web Montag Dresden Event 1',
               happens_at: 6.weeks.from_now,
               organizer: User.all.second,
               link: 'http://',
-              twitter_id: Array.new(32) { rand(36).to_s(36) }.join
+              twitter_id: fake_twitter_id
 
 Event.create! text: 'Web Montag Dresden Event 2',
-              happens_at: 6.weeks.from_now,
+              happens_at: 12.weeks.from_now,
               organizer: User.all.second,
               link: 'http://',
-              twitter_id: Array.new(32) { rand(36).to_s(36) }.join
+              twitter_id: fake_twitter_id
 
 
 # User.create! username: 'admin',
