a8c6f8376b72c3e1eec6dee56ff85500bc2e2961
Thomas Bachmann <thomas.bachmann@mambu.com>
2014-03-06 21:32:36 +0100

    #12 Events als Liste (embeddable) - Liste verschönert und von session auf get parameter umgestellt

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,14 +8,14 @@ class Event < ActiveRecord::Base
   scope :oldest_first, -> { order('happens_at asc') }
   scope :newest_first, -> { order('happens_at desc') }
 
-  scope :for_organizer, -> (org) { where('user_id=?', org) if org.present? }
-
+  scope :for_organizer, -> (org) { where('user_id=?', org.id) if org.present? }
+  scope :for_organizers, -> (orgs) { where('user_id IN (?)', orgs.map{|org| org.id}) if orgs.present? }
   belongs_to :organizer, foreign_key: :user_id
 
   validates_presence_of :link
   validates_presence_of :text
   validates_presence_of :happens_at
-  validates_presence_of :organizer
+  validates_presence_of :user_id
   validates_length_of   :link, :maximum => 200
   validates_length_of   :text, :maximum => 200
 end
