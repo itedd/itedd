68b86fc8ea822bed4f4b89c224d2cb78aff830c6
Thomas Bachmann <thomas.bachmann@mambu.com>
2014-03-06 22:03:44 +0100

    #12 Events als Liste (embeddable) - fixed broken junit test

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,7 +15,7 @@ class Event < ActiveRecord::Base
   validates_presence_of :link
   validates_presence_of :text
   validates_presence_of :happens_at
-  validates_presence_of :user_id
+  validates_presence_of :organizer
   validates_length_of   :link, :maximum => 200
   validates_length_of   :text, :maximum => 200
 end
