cf22675a050247e058c0322538c7964e37988f74
Paul Fritsche <synopia@gmx.net>
2014-03-06 14:06:16 +0100

    #5, List organizations page

--- app/models/organizer.rb
+++ app/models/organizer.rb
@@ -7,7 +7,7 @@ class Organizer < User
   validates :googleplus_page, length: {maximum: 200}
 
   scope :with_twitter, -> { where('twitter_account is not null') }
-  has_many :events, foreign_key: :user_id , order:'happens_at DESC'
+  has_many :events, -> { order('happens_at desc') }, foreign_key: :user_id
 
   validates :twitter_account, length: {maximum: 200},
     format: { with: %r{\A(\@\w+|)\z} }, if: ->(r) { r.twitter_account.present? }
