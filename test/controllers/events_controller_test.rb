310633fe3e7230b277904899fff3ccad2cdb9b83
Paul Fritsche <paul.fritsche@gmail.com>
2014-03-08 12:37:10 +0100

    fixed tests

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class EventsControllerTest < ActionController::TestCase
 
-  test "should render show" do
-    get :show
+  test "should render embedded without login" do
+    get :embedded
     assert_response :success, @response.body
   end
 
--- test/controllers/user_groups_controller_test.rb
+++ test/controllers/user_groups_controller_test.rb
@@ -3,14 +3,21 @@ require 'test_helper'
 class UserGroupsControllerTest < ActionController::TestCase
 
   test "should redirect to sign in if not logged in" do
-    get :edit
+    get :edit, :id=>1
     assert_redirected_to new_user_session_path
   end
 
-  test "should render edit" do
-    sign_in User.first
-    get :edit, :format=>1
+  test "should render edit for organizer" do
+    sign_in users(:valid_organizer)
+    get :edit, :id=>1
     assert_response :success, @response.body
+  end
+
+  test "should not render edit for normal user" do
+    sign_in users(:valid_user)
+    assert_raise CanCan::AccessDenied do
+      get :edit, :id=>1
+    end
   end
 
 end
