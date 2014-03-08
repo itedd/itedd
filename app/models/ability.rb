3d1a64df5fd39dcbbc39635889e7db1ce7ccbd20
Paul Fritsche <paul.fritsche@gmail.com>
2014-03-08 12:19:12 +0100

    Edit events fixed resources routes fixed authorization issues

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,6 +8,9 @@ class Ability
     else
       can :read, :all
       can :manage, UserGroup, :user_id=>user.id
+      UserGroup.where(user_id: user.id).each do |user_group|
+        can :manage, Event, :user_group_id=>user_group.id
+      end
     end
   end
 end
