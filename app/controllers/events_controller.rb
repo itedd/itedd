421b95c723d9b0d052b04b14a5ce9da3353184f4
Martin Schrader <sonnendeck2000@gmail.com>
2014-04-06 20:18:05 +0200

    Updated design

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,11 +18,13 @@ class EventsController < ApplicationController
   def embedded
     @page = params[:page].to_i
     @per = (params[:per] || 10).to_i
+
     if not params[:user_group_ids].blank?
       @user_groups = UserGroup.find(params[:user_group_ids])
     elsif not params[:user_group_id].blank?
       @user_groups = UserGroup.where('id=?', params[:user_group_id])
     end
+
     if @page>=0
       @events = Event.upcoming_events.for_user_groups(@user_groups).limit(@per).offset(@page*@per)
     else
