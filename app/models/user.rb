f1c71b5e84e49a44dafe880a38b05c5a242cf298
Paul Fritsche <synopia@gmx.net>
2014-04-07 17:07:11 +0200

    Benutzerliste zum Verwalten von Usern (freischalten, sperren, Admin vergeben/entfernen)

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
 
   accepts_nested_attributes_for :user_group
 
+  default_scope order('email ASC')
+
   def self.new_with_session(hash, session)
     hash.delete 'user_group_attributes' if hash['user_group_id'].present?
     user = super
