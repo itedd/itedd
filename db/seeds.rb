ca0322ba6e01ad77559cbd98d0176dec848d1ce7
synopia <paul.fritsche@googlemail.com>
2014-04-10 17:45:49 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-jugsaxony = UserGroup.create! name: 'JUG Saxony', twitter_account: '@jugsaxony', color: '#ffffff'
-agile_saxony = UserGroup.create! name: 'Agile Saxony', twitter_account: '@AgileSaxony', color: '#ffffff'
-cofab = UserGroup.create! name: 'CoFab DD', twitter_account: '@cofabdd', color: '#ffffff'
-rug = UserGroup.create! name: 'ruby-dresden', twitter_account: '@ruby_dresden', color: '#ffffff'
-webmontag = UserGroup.create! name: 'WebMontag Dresden', twitter_account: '@webmontagdd',color: '#ffffff'
-wdmc = UserGroup.create! name: 'WDMC Dresden', twitter_account: '@wdmcdresden', color: '#ffffff'
-barcamp = UserGroup.create! name: 'BarCamp Dresden', twitter_account: '@startcampdd', color: '#ffffff'
-output =  UserGroup.create! name: 'Output Dresden', twitter_account: '@outputdd', color: '#ffffff'
-agile_hardware =  UserGroup.create! name: 'Agile Hardware', twitter_account: '@agile_hardware', color: '#ffffff'
+jugsaxony      = UserGroup.create! name: 'JUG Saxony',        twitter_account: '@jugsaxony',      color: '#ffffff'
+agile_saxony   = UserGroup.create! name: 'Agile Saxony',      twitter_account: '@AgileSaxony',    color: '#ffffff'
+cofab          = UserGroup.create! name: 'CoFab DD',          twitter_account: '@cofabdd',        color: '#ffffff'
+rug            = UserGroup.create! name: 'ruby-dresden',      twitter_account: '@ruby_dresden',   color: '#ffffff'
+webmontag      = UserGroup.create! name: 'WebMontag Dresden', twitter_account: '@webmontagdd',    color: '#ffffff'
+wdmc           = UserGroup.create! name: 'WDMC Dresden',      twitter_account: '@wdmcdresden',    color: '#ffffff'
+barcamp        = UserGroup.create! name: 'BarCamp Dresden',   twitter_account: '@startcampdd',    color: '#ffffff'
+output         = UserGroup.create! name: 'Output Dresden',    twitter_account: '@outputdd',       color: '#ffffff'
+agile_hardware = UserGroup.create! name: 'Agile Hardware',    twitter_account: '@agile_hardware', color: '#ffffff'
 
 User.create! email: 'paul.fritsche@objectfab.de',
              password: 'password',
