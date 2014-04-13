9136f286a00f4bbd2de9a2531eca221c27096168
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2014-04-13 23:56:51 +0200

    cleaned user_group_admins controller
    * using cancan

--- app/controllers/user_group_admins_controller.rb
+++ app/controllers/user_group_admins_controller.rb
@@ -1,20 +1,16 @@
 class UserGroupAdminsController < ApplicationController
+  before_filter :load_and_authorize_user_group
+
   def index
-    authorize! :manage, UserGroup
-    @user_groups = UserGroup.accessible_by(current_ability, :manage)
     if @user_groups.size==1
       redirect_to controller: :user_groups, action: :show, id: @user_groups.first.id
     end
   end
 
   def edit
-    @user_group = UserGroup.find(params[:id])
-    authorize! :manage, @user_group
   end
 
   def update
-    @user_group = UserGroup.find(params[:id])
-    authorize! :manage, @user_group
     if @user_group.update_attributes(user_group_params)
       redirect_to :user_group_admins
     else
@@ -24,8 +20,15 @@ class UserGroupAdminsController < ApplicationController
 
   private
 
+  def load_and_authorize_user_group
+    old_action = params[:action]
+    params[:action] = :manage
+    self.class.cancan_resource_class.new(self, nil, class: 'UserGroup', instance_name: :user_group).load_and_authorize_resource
+  ensure
+    params[:action] = old_action
+  end
+
   def user_group_params
     params.require(:user_group).permit(:name, :color, :logo, :website, :description, :facebook_page, :googleplus_page, :twitter_account)
   end
 end
-
