2275c9fdb121f8d60755eb9d29e437cfd1d1dc08
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2014-04-13 23:52:54 +0200

    added cannot for self managing users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,13 +4,14 @@ class Ability
   def initialize(user)
     user ||= User.new
     if user.admin?
-      can :manage, :all
+<      can :manage, :all
+      cannot :manage, User, id: user.id
     else
       can :read, :all
-      ug = user.user_group
-      if ug.present?
-        can :manage, UserGroup, :id=>ug.id
-        can :manage, Event, :user_group_id=>ug.id
+      user_group = user.user_group
+      if user_group.present?
+        can :manage, UserGroup, id: user_group.id
+        can :manage, Event, user_group_id: user_group.id
       end
     end
   end
