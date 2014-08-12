79ce42fec0c1913e8dd379d2de36e692654850e8
Paul Fritsche <synopia@gmx.net>
2014-08-12 14:53:59 +0200

    use creation date of tweet as 'now' for parsing dates

--- app/services/base_reader.rb
+++ app/services/base_reader.rb
@@ -1,14 +1,14 @@
 class BaseReader
   include ExtractDate
 
-  def build_event(event_text, user_group, optional_link=nil)
+  def build_event(event_text, user_group, tweet_url, tweet_time)
     raise ArgumentError.new("No user_group given") unless user_group.present?
 
     if is_valid_text?(event_text)
       Event.new( {
         text: event_text,
-        happens_at: extract_date(event_text),
-        link: optional_link, #|| extract_link(event_text),
+        happens_at: extract_date(event_text, tweet_time),
+        link: tweet_url,
         user_group: user_group })
     end
   end
