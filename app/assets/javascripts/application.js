89b8c77ca4802238d463aa585c1eda20b62b3691
pebra <self@pebra.net>
2015-09-28 13:52:22 +0200

    BUGFIX: We have to explicitly require JS files from vender/assets in application.js app/assets/javascripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
+//= require jquery.eventCalendar.min.js
+//= require jquery.timeago.js
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
