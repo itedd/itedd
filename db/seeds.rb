6c6720322335a809c8c1d60f9e10d37345f5fd3d
pebra <self@pebra.net>
2015-10-01 11:09:36 +0200

    Removed `is_mobile_device?` helper in favor of using media queries. Cleanup. Added more data to seeds.rb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@ user = User.where(email: 'steffen.gemkow@objectfab.de').first_or_create!(
 
 user.user_group ||= UserGroup.create! name: 'Ruby Dresden', website: 'http://www.ruby-dresden.de', twitter_account: '@ruby_dresden', color: '#000000'
 user.save
+
+events = (1..20).map { |n| { text: "Ruby Meetup No. #{n}", link: "http://google.com", happens_at: Date.today + n.days}}
+
+user.user_group.events.build(events)
+user.user_group.save
